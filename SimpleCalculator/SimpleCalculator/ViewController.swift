6aee0e830e12d64f5b9cb3f4d432940cf1982d7a
BillyConway256 <115131554+BillyConway256@users.noreply.github.com>
2025-02-02 13:57:31 -0600

    FEAT: Completed a non functional calculator design

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Billy Conway on 2/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
